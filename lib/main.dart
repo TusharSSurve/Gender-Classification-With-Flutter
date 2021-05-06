8103de3496fc5776549e08e0d8235f3510cda3ab
Tushar Surve <ttusharsurve567@gmail.com>
2021-05-06 18:33:08 +0530

    Gender classification with flutter

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _isLoading;
+  File _image;
+  List _output;
+
+  @override
+  void initState() {
+    _isLoading = true;
+    loadModel().then((value) {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
+  chooseImage() async {
+    var image = await ImagePicker().getImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _isLoading = true;
+      _image = File(image.path);
+    });
+    runModelOnImage();
+  }
+
+  Future<String> loadModel() async {
+    return await Tflite.loadModel(
+        model: 'assets/model/fm.tflite', labels: 'assets/model/labels.txt');
+  }
+
+  runModelOnImage() async {
+    var output = await Tflite.runModelOnImage(
+        path: _image.path,
+        numResults: 2,
+        imageMean: 0.0,
+        imageStd: 255.0,
+        threshold: 0.5);
+
+    setState(() {
+      _isLoading = false;
+      _output = output;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gender Classification'),
+      ),
+      body: _isLoading
+          ? Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              child: Column(
+                children: [
+                  _image == null ? Container() : Image.file(_image),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  _output == null ? Text('') : Text('${_output[0]["label"]}')
+                ],
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.camera,
+          size: 40,
+        ),
+        onPressed: () {
+          chooseImage();
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+}
